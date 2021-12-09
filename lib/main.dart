fbcc317aa2bfe2733cb68aec338c3e647fccea8a
MuhammadDhiyaulAuliya <59078451+MuhammadDhiyaul@users.noreply.github.com>
2021-12-09 14:03:27 +0700

    Flutter 19 - Login Resgister

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:demo_app/screens/login.dart';
+import 'package:demo_app/screens/register.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -7,43 +9,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Login'),
-        ),
-        body: Container(
-          color: Theme.of(context).primaryColorDark,
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Card(
-                  elevation: 8,
-                  margin: EdgeInsets.only(left: 16.0, right: 16.0),
-                  child: Padding(
-                    padding: EdgeInsets.all(16.0),
-                    child: Column(
-                      children: <Widget>[
-                        TextField(
-                          keyboardType: TextInputType.emailAddress,
-                          decoration: InputDecoration(labelText: 'Email'),
-                        ),
-                        TextField(
-                          keyboardType: TextInputType.visiblePassword,
-                          decoration: InputDecoration(labelText: 'Password'),
-                        ),
-                        ElevatedButton(
-                          onPressed: () => print('Login clicked'),
-                          child: Text('Login'),
-                          style: ElevatedButton.styleFrom(
-                              minimumSize: Size(double.infinity, 36)),
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-              ]),
-        ),
-      ),
+      home: Login(),
+      routes: {
+        '/login': (context) => Login(),
+        '/register': (context) => Register(),
+      },
     );
   }
 }
